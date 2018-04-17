b4ea19e60ad67574c70cf7deb8e55ee27e4e2399
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2018-04-17 08:06:05 +0100

    Improved storing of chromedriver and pathing

--- sharedStepDemo/src/main/java/driverfactory/DriverFactory.java
+++ sharedStepDemo/src/main/java/driverfactory/DriverFactory.java
@@ -7,7 +7,7 @@ public class DriverFactory {
     // Download from: https://sites.google.com/a/chromium.org/chromedriver/downloads
 
     public ChromeDriver generateDriver(){
-        String pathToChromeDriver = System.getProperty("user.dir") + "/src/main/java/driverfactory/chromedriver";
+        String pathToChromeDriver = "lib/chromedriver";
         System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
 
         return new ChromeDriver();
